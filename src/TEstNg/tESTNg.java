b22afce12bd1c76640424355726548751ea053ee
aartiMogal28@ <aartimogal2002@gmail.com>
2024-12-21 21:52:29 +0530

    Created new file

---
+++ src/TEstNg/tESTNg.java
@@ -0,0 +1,51 @@
+package TEstNg;
+
+import org.testng.annotations.Test;
+
+public class tESTNg {
+
+	@Test
+	public void CheckValidUser()
+	{
+		System.out.println("this is form checkValidUser");
+		
+	}
+	@Test
+	public void checkInvalidUser()
+	{
+		System.out.println("this is from CheckInvaliduser");
+	}
+	@Test
+	public void abc()
+	{
+		System.out.println("this is abc");
+	}
+	@Test
+	public void xyz()
+	{
+		System.out .println("this is xyz");
+		
+	}
+	public void ijk()
+	{
+		System.out.println("this is ijk");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
